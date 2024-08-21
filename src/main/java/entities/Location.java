b74743b67b9f5997780413a04627c9a8fe4d5633
C043 <mariofragnitoph@gmail.com>
2024-08-21 15:26:10 +0200

    Adds PersonDAO

--- src/main/java/entities/Location.java
+++ src/main/java/entities/Location.java
@@ -1,13 +1,11 @@
 package entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
 @Entity
+@Table(name = "locations")
 public class Location {
     @Id
     @GeneratedValue
